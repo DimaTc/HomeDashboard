52941cdfc8e1e329778e30d3cfcf7b52f61990b9
Tchenak, Yakov <yakov_tchenak@bmc.com>
2016-06-05 22:49:26 +0300

    fix 404

--- Server/server.py
+++ Server/server.py
@@ -1,7 +1,7 @@
 import socket
 import threading
 import os
-
+import sys
 
 default_error_page = """\
 <!DOCTYPE HTML>
@@ -26,12 +26,14 @@ default_header_content_type = "Content-Type: text/html; charset=utf-8\r\n\r\n"
 buffer_size = 1024
 
 
-def get_page(code):
-    page = default_error_page
+def get_page(code,status):
+    page = default_error_page % {'code': code, 'status': status}
     if code == 200:
         pass
     else:
-        file = open(os.path.dirname(__file__) + "/www/not-found.html", 'r')
+        #http://www.blog.pythonlibrary.org/2013/10/29/python-101-how-to-find-the-path-of-a-running-script/
+        cwd = os.path.abspath(os.path.dirname(sys.argv[0]))
+        file = open(cwd + "/www/not-found.html", 'r')
         page = file.read()
     return page
 
@@ -39,21 +41,26 @@ class BaseServer:
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_name = ""
-    host_name = ""
+    host_name = "0.0.0.0"
     host_port = 8000  # default port
     is_shutdown = False
 
     def __init__(self, name):
         self.server_name = name
 
+        
     def start_server(self):
-        thread = threading.Thread(target=self.run_server(), name=self.server_name)
+        print('server starting...')
+        thread = threading.Thread(target=self.run_server, name=self.server_name)
+        print('starting main server thread...')
         thread.start()
-        pass
+
+
 
     def run_server(self):
+        print('Serving HTTP on %s port %s ...' % (self.host_name, self.host_port))
         self.server_socket.bind((self.host_name, self.host_port))  # bind to host
-        self.server_socket.listen()
+        self.server_socket.listen(5)
         while not self.is_shutdown:
             conn, addr = self.server_socket.accept()
             self.handle_request(conn, addr)
@@ -62,7 +69,7 @@ class BaseServer:
         pass
 
     def handle_request(self, connection, address):
-        print(str(address[0]) + " Connected! (port " + str(address[1]) + ")")
+        print('Connected from %s:%d' % (address[0], address[1]))
         result = self.parse_request(connection.recv(buffer_size))
         if result == 0:
             page = self.parse_response(200)
@@ -96,9 +103,8 @@ class BaseServer:
         base_header = (default_header_status % {'code': code, 'status': status})
         base_content_type = default_header_content_type
         # page = (default_error_page % {'code': code, 'status': status})
-        page = str(get_page(code))
+        page = str(get_page(code, status))
         return_string = str(base_header + base_content_type + page).encode('utf-8')
-        print(return_string)
         return return_string
 
     def check_if_shutdown(self):
@@ -108,8 +114,9 @@ class BaseServer:
 def main():
     server = BaseServer("Home Server")
     server.start_server()
-    pass
+    
 
 
+    
 if __name__ == "__main__":
     main()
