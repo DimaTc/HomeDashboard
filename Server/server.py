18ab9f118e47e5d5706fa2350b532681f1e2fe78
FlashFlush <Dimatch1@gmail.com>
2016-06-06 21:43:59 +0300

    Fix the GET issue for getting files

--- Server/server.py
+++ Server/server.py
@@ -26,28 +26,33 @@ default_header_content_type = "Content-Type: text/html; charset=utf-8\r\n\r\n"
 buffer_size = 1024
 
 
-def get_page(code,status):
-    page = default_error_page % {'code': code, 'status': status}
-    if code == 200:
-        pass
-    else:
-        #http://www.blog.pythonlibrary.org/2013/10/29/python-101-how-to-find-the-path-of-a-running-script/
-        cwd = os.path.abspath(os.path.dirname(sys.argv[0]))
-        file = open(cwd + "/www/not-found.html", 'r')
-        page = file.read()
-    return page
+# def get_page(path):
+
+    #http://www.blog.pythonlibrary.org/2013/10/29/python-101-how-to-find-the-path-of-a-running-script/
+
+
+def get_file(path):
+    file_io = open(path.replace("//", "/"), 'rb')
+    file = file_io.read()
+    return file
+
 
 class BaseServer:
 
     server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     server_name = ""
+    server_path = ""
+    server_www_path = ""
+
     host_name = "0.0.0.0"
-    host_port = 8000  # default port
+
+    host_port = 8001  # default port
     is_shutdown = False
 
     def __init__(self, name):
         self.server_name = name
-
+        self.server_path = os.path.dirname(sys.argv[0])
+        self.server_www_path = self.server_path + "/www"
         
     def start_server(self):
         print('server starting...')
@@ -70,12 +75,27 @@ class BaseServer:
 
     def handle_request(self, connection, address):
         print('Connected from %s:%d' % (address[0], address[1]))
-        result = self.parse_request(connection.recv(buffer_size))
-        if result == 0:
-            page = self.parse_response(200)
+        media = [False, ""]
+        code, file_path = self.parse_request(connection.recv(buffer_size))
+        full_path = self.server_www_path + "/" + file_path
+        if str(file_path).endswith(tuple([".png", ".jpg", ".gif"])):
+            if str(file_path).endswith(".gif"):
+                media[1] = "gif"
+            else:
+                media[1] = "jpeg"
+
+            page = get_file(full_path)
+            media[0] = True
         else:
-            page = self.parse_response(404)
-        connection.sendall(bytes(page))
+            page = get_file(full_path).decode("utf-8")
+
+        response_headers, data = self.parse_response(code, page, media)
+        if type(data) == bytes:
+            connection.send(bytes(response_headers.encode("utf-8")))
+            connection.sendall(data)
+        else:
+            connection.send(bytes((response_headers + data).encode("utf-8")))
+
 
     def parse_request(self, data):
         if len(data) == 0:
@@ -86,26 +106,39 @@ class BaseServer:
         print("path - " + path)
         print("version - " + version)
 
-        status = 1
-
+        # status = 1
+        file = "/not-found.html"
+        code = 200
         if path == "/":
-            status = 0
+            file = "/index.html"
+        elif self.is_file_exist(path):
+            file = path
+        else:
+            code = 404
 
-        return status
+        return code, file
 
-    def parse_response(self, code):
+
+    def parse_response(self, code, page, media):
         status = "ERROR"
+
         if code == 200:
             status = "OK"
         elif code == 404:
             status = "NOT FOUND"
-
         base_header = (default_header_status % {'code': code, 'status': status})
-        base_content_type = default_header_content_type
-        # page = (default_error_page % {'code': code, 'status': status})
-        page = str(get_page(code, status))
-        return_string = str(base_header + base_content_type + page).encode('utf-8')
-        return return_string
+        if not media[0]:
+            base_content_type = default_header_content_type
+        else:
+            base_content_type = "Content-Type: image/"+media[1]+"\r\n\r\n"
+
+        # return_string = str(base_header + base_content_type + page)
+
+        return base_header + base_content_type, page
+
+    def is_file_exist(self, path):
+        full_path = self.server_www_path + path
+        return os.path.isfile(full_path)
 
     def check_if_shutdown(self):
         pass
@@ -116,7 +149,5 @@ def main():
     server.start_server()
     
 
-
-    
 if __name__ == "__main__":
     main()
